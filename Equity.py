1feb73921f128a126a608760b41cd1a5c06854a1
Nikhil Kulkarni <nikhil896@gmail.com>
2017-04-04 12:33:05 -0400

    Initial implementation of scraper for briefing.com

---
+++ Equity.py
@@ -0,0 +1,8 @@
+class Equity(object):
+
+    def __init__(self, name, ticker, brokerage, previousRating, currentRating):
+        self.name = name
+        self.ticker = ticker
+        self.brokerage = brokerage
+        self.previousRating = previousRating
+        self.currentRating = currentRating
---
+++ scraper.py
@@ -0,0 +1,34 @@
+import urllib2
+import re
+from datetime import date
+from bs4 import BeautifulSoup
+from Equity import Equity
+
+# Get today'a upgrades
+def getTodaysEquities():
+    today = date.today()
+    content = urllib2.urlopen("https://www.briefing.com/Investor/Calendars/Upgrades-Downgrades/Upgrades/" + str(today.year) + "/" + str(today.month) + "/" + str(today.day)).read()
+
+    soup = BeautifulSoup(content, "html5lib")
+    # Find all equities
+    upgrades = soup.find_all("tr", {"class" : ["wh-row", "hl-row"]})
+
+    # List of equities that have changed analyst ratings
+    equities = []
+
+    for equity in upgrades:
+        equityInfo = equity.find_all("td")
+        name = equityInfo[0].text
+        ticker = equityInfo[1].text
+        brokerage = equityInfo[2].text
+        ratingChange = equityInfo[3].text.split(u"\u00BB")
+        previousRating = ratingChange[0]
+        currentRating = ratingChange[1]
+
+        # Append Equity to object to array
+        equities.append(Equity(name, ticker, brokerage, previousRating, currentRating))
+
+    # for equity in equities:
+    #     print(equity.previousRating)
+
+getTodaysEquities()
