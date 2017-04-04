543d3734b3135f42f6fa34dd13e28be068a11e59
Nikhil Kulkarni <nikhil896@gmail.com>
2017-04-04 12:59:39 -0400

    Added comment for my trading strategy

--- trader.py
+++ trader.py
@@ -9,3 +9,10 @@ for equity in equities:
     print(share.get_price())
 
 # Implement trading strategy here
+# Strategy Idea 1:
+# 1. Buy the stock with the most common buy rating
+# 2. Sell entire portfolio the next morning and repeat
+
+
+#Strategy Idea 2:
+# 1.
