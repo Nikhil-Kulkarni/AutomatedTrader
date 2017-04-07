399d4f14542c9cb7b3d39aa9940f64555f87c3e8
Nikhil Kulkarni <nikhil896@gmail.com>
2017-04-06 22:56:02 -0400

    Connected to dynamodb. Will monitor for a week

--- trader.py
+++ trader.py
@@ -1,18 +1,50 @@
 import scraper
 from Equity import Equity
+from DecimalEncoder import DecimalEncoder
 from yahoo_finance import Share
+from datetime import date
+import boto3
+import math
+import decimal
 
-equities = scraper.getTodaysEquities()
-
-for equity in equities:
-    share = Share(str(equity.ticker))
-    print(share.get_price())
+# Save today's buys and sells
+dynamodb = boto3.resource('dynamodb')
 
 # Implement trading strategy here
 # Strategy Idea 1:
 # 1. Buy the stock with the most common buy rating
 # 2. Sell entire portfolio the next morning and repeat
+def trade(money):
+    equities = scraper.getTodaysEquities()
+    equitiesBuyRating = []
+    stocks = dynamodb.Table('Stocks')
 
+    for equity in equities:
+        if str(equity.currentRating) == " Buy":
+            equitiesBuyRating.append(str(equity.ticker))
 
-#Strategy Idea 2:
-# 1.
+    # Most common BUY Rating
+    mostCommonBuyTicker = max(set(equitiesBuyRating), key=equitiesBuyRating.count)
+    share = Share(mostCommonBuyTicker)
+    priceOfShare = float(share.get_price())
+
+    # Compute the number of shares I can buy
+    numberOfShares = math.floor(money/priceOfShare)
+
+    # Timestamp
+    today = date.today()
+    timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
+
+    # Save to db
+    response = stocks.put_item(
+        Item={
+            'ticker':mostCommonBuyTicker,
+            'timestamp':timestamp,
+            'price':decimal.Decimal(str(priceOfShare)),
+            'shares':decimal.Decimal(numberOfShares),
+            'transactionType':'BUY'
+        }
+    )
+
+# initialize()
+trade(1000)
