000d48b67dbf8c1e343a7d2ebce4fafeed444d38
Nikhil Kulkarni <nikhil896@gmail.com>
2017-04-07 23:10:16 -0400

    Ran both strategies fora  year

--- strategyTwo.py
+++ strategyTwo.py
@@ -1,11 +1,12 @@
 import scraper
 from Equity import Equity
 from DecimalEncoder import DecimalEncoder
-from datetime import date
+from datetime import timedelta, date
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 from googlefinance import getQuotes
 from yahoo_finance import Share
+from yahoo_finance import YQLResponseMalformedError
 import boto3
 import math
 import decimal
@@ -18,107 +19,142 @@ dynamodb = boto3.resource('dynamodb')
 # Strategy Idea 1:
 # 1. Buy the stock with the most common buy rating
 # 2. Sell entire portfolio the next morning and repeat
-def trade(money, currentDate):
-    equities = scraper.getTodaysEquities(currentDate)
-    equitiesBuyRating = []
-    stocks = dynamodb.Table('Stocks')
 
-    for equity in equities:
-        if str(equity.currentRating) == " Strong Buy" or str(equity.currentRating) == " Outperform" or str(equity.currentRating) == " Sector Outperform" or str(equity.currentRating) == " Mkt Outperform":
-            equitiesBuyRating.append(equity)
+class StrategyTwo(object):
 
-    listOfPurchases = []
-    topStocksToPurchase = equitiesBuyRating[:4]
+    def __init__(self):
+        self.deltaDays = 0
 
-    # Take 4 4 stocks and buy them
-    for stock in topStocksToPurchase:
-        amountInvestedPerStock = float(money / len(topStocksToPurchase))
+    def trade(self, money, currentDate):
+        equities = scraper.getTodaysEquities(currentDate)
+        equitiesBuyRating = []
+        stocks = dynamodb.Table('Stocks')
 
-        ticker = stock.ticker
-        share = Share(str(ticker))
-        priceOfShare = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+        for equity in equities:
+            if str(equity.currentRating) == " Strong Buy" or str(equity.currentRating) == " Outperform" or str(equity.currentRating) == " Sector Outperform" or str(equity.currentRating) == " Mkt Outperform":
+                equitiesBuyRating.append(equity)
 
-        # Compute the number of shares of this stock I can buy
-        numberOfShares = math.floor(amountInvestedPerStock/priceOfShare)
-        listOfPurchases.append({'price':decimal.Decimal(str(priceOfShare)), 'shares':decimal.Decimal(str(numberOfShares)), 'ticker':str(ticker)})
+        listOfPurchases = []
+        topStocksToPurchase = equitiesBuyRating[:4]
 
-    # Buy the shares and save to DB
-    timestamp = str(currentDate.year) + "-" + str(currentDate.month) + "-" + str(currentDate.day)
+        # Take 4 4 stocks and buy them
+        for stock in topStocksToPurchase:
+            amountInvestedPerStock = float(money / len(topStocksToPurchase))
 
-    # Save to db
-    response = stocks.put_item(
-        Item={
-            'transactionType':'BUY',
-            'timestamp':timestamp,
-            'listOfPurchases':listOfPurchases,
-            'bank':decimal.Decimal(str(money))
-        }
-    )
+            ticker = stock.ticker
+            share = Share(str(ticker))
+            # print ticker
 
-# Sell entire portfolio
-def sell(currentDate):
-    stocks = dynamodb.Table('Stocks')
+            try:
+                priceOfShare = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+                # Compute the number of shares of this stock I can buy
+                numberOfShares = math.floor(amountInvestedPerStock/priceOfShare)
+                listOfPurchases.append({'price':decimal.Decimal(str(priceOfShare)), 'shares':decimal.Decimal(str(numberOfShares)), 'ticker':str(ticker)})
+            except YQLResponseMalformedError as e:
+                print e
+                self.deltaDays += 1
 
-    today = currentDate
-    timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 1)
+        # Buy the shares and save to DB
+        timestamp = str(currentDate.year) + "-" + str(currentDate.month) + "-" + str(currentDate.day)
 
-    # January
-    # if (currentDate.day in [9, 16, 23, 30]):
-    # February/March:
-    if (currentDate.day in [6, 13, 20, 27]):
-        timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 3)
+        if (len(listOfPurchases) != 0):
+            # Save to db
+            response = stocks.put_item(
+                Item={
+                    'transactionType':'BUY',
+                    'timestamp':timestamp,
+                    'listOfPurchases':listOfPurchases,
+                    'bank':decimal.Decimal(str(money))
+                }
+            )
+            self.deltaDays = 0
+        else:
+            self.deltaDays += 1
 
-    # January
-    # if (currentDate.day in [17]):
-    # February:
-    # if (currentDate.day in [21]):
-    #     timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 4)
+    # Sell entire portfolio
+    def sell(self, currentDate):
+        stocks = dynamodb.Table('Stocks')
 
-    try:
-        response = stocks.get_item(
-            Key={
-                'timestamp': timestamp,
-                'transactionType':'BUY'
-            }
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        purchase = response['Item']
-        itemsPurchased = purchase['listOfPurchases']
-
-        portfolioValue = 0
-        amountNotInvested = 0
-        listOfSells = []
-
-        for item in itemsPurchased:
-            numberShares = item['shares']
-            ticker = item['ticker']
-            share = Share(str(item['ticker']))
-            lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
-            portfolioValue = portfolioValue + float(lastTradePrice) * float(numberShares)
-            amountNotInvested = amountNotInvested + (float(purchase['bank']/len(itemsPurchased)) - float(numberShares) * float(item['price']))
-            listOfSells.append({'price':decimal.Decimal(str(lastTradePrice)), 'shares':decimal.Decimal(str(numberShares)), 'ticker':str(ticker)})
-
-        newBankValue = portfolioValue + amountNotInvested
-        print("Net gain: " + str(newBankValue - float(purchase['bank'])))
-        print("Bank: " + str(newBankValue))
-
-        # Timestamp
-        today = currentDate
+        today = currentDate - timedelta(days=(1+self.deltaDays))
         timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
 
-        # Save to db
-        response = stocks.put_item(
-            Item={
-                'transactionType':'BUY',
-                'timestamp':timestamp,
-                'listOfPurchases':listOfSells,
-                'bank':decimal.Decimal(str(newBankValue))
-            }
-        )
+        if (currentDate.weekday() == 0):
+            today = currentDate - timedelta(days=(3+self.deltaDays))
+            timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
 
-        trade(newBankValue, currentDate)
+        try:
+            response = stocks.get_item(
+                Key={
+                    'timestamp': timestamp,
+                    'transactionType':'BUY'
+                }
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print currentDate
+            print timestamp
+            purchase = response['Item']
+            itemsPurchased = purchase['listOfPurchases']
+
+            portfolioValue = 0
+            amountNotInvested = 0
+            listOfSells = []
+
+            share = Share(itemsPurchased[0]['ticker'])
+
+            try:
+                lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+            except YQLResponseMalformedError as e:
+                if (currentDate.weekday() == 0):
+                    self.deltaDays = self.deltaDays + 3
+                else:
+                    self.deltaDays = self.deltaDays + 1
+                return
+
+            for item in itemsPurchased:
+                numberShares = item['shares']
+                ticker = item['ticker']
+                share = Share(str(item['ticker']))
+                lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+                portfolioValue = portfolioValue + float(lastTradePrice) * float(numberShares)
+                amountNotInvested = amountNotInvested + (float(purchase['bank']/len(itemsPurchased)) - float(numberShares) * float(item['price']))
+                listOfSells.append({'price':decimal.Decimal(str(lastTradePrice)), 'shares':decimal.Decimal(str(numberShares)), 'ticker':str(ticker)})
+
+            newBankValue = portfolioValue + amountNotInvested
+            print("Net gain: " + str(newBankValue - float(purchase['bank'])))
+            print("Bank: " + str(newBankValue))
+
+            # Timestamp
+            today = currentDate
+            timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
+
+            # Save to db
+            response = stocks.put_item(
+                Item={
+                    'transactionType':'SELL',
+                    'timestamp':timestamp,
+                    'listOfPurchases':listOfSells,
+                    'bank':decimal.Decimal(str(newBankValue))
+                }
+            )
+
+            self.trade(newBankValue, currentDate)
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+trader = StrategyTwo()
+
+start = date(2016, 4, 8)
+trader.trade(1000, start)
+start_date = date(2016, 4, 9)
+end_date = date(2017, 4, 6)
+for single_date in daterange(start_date, end_date):
+    if single_date.weekday() < 5:
+        # print single_date.strftime("%Y-%m-%d")
+        trader.sell(single_date)
 
 # Strategy Idea 2:
 # 1. Choose 4 stocks with strong buy or outperform rating
@@ -127,7 +163,7 @@ def sell(currentDate):
 
 # Trade - January
 # initialDate = date(2017, 1, 3)
-# initialBank = 3000
+# initialBank = 1000
 # trade(initialBank, initialDate)
 # for day in range(4, 32) :
 #     if day not in [7, 8, 14, 15, 16, 21, 22, 28, 29]:
@@ -136,19 +172,19 @@ def sell(currentDate):
 
 # Trade - February
 # initialDate = date(2017, 2, 1)
-# initialBank = 3655.879601
+# initialBank = 1018.339993
 # trade(initialBank, initialDate)
 # for day in range(2, 29) :
 #     if day not in [4, 5, 11, 12, 18, 19, 20, 25, 26]:
 #         currentDate = date(2017, 2, day)
 #         sell(currentDate)
 
-# # Trade - March
-initialDate = date(2017, 3, 1)
-initialBank = 1000
-trade(initialBank, initialDate)
-# sell(initialDate)
-for day in range(2, 32) :
-    if day not in [4, 5, 11, 12, 18, 19, 25, 26]:
-        currentDate = date(2017, 3, day)
-        sell(currentDate)
+# Trade - March
+# initialDate = date(2017, 3, 1)
+# initialBank = 75081.430085
+# trade(initialBank, initialDate)
+# # sell(initialDate)
+# for day in range(2, 32) :
+#     if day not in [4, 5, 11, 12, 18, 19, 25, 26]:
+#         currentDate = date(2017, 3, day)
+#         sell(currentDate)
--- trader.py
+++ trader.py
@@ -1,11 +1,11 @@
 import scraper
 from Equity import Equity
 from DecimalEncoder import DecimalEncoder
-from datetime import date
+from datetime import timedelta, date
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 from googlefinance import getQuotes
-from yahoo_finance import Share
+from yahoo_finance import Share, YQLResponseMalformedError
 import boto3
 import math
 import decimal
@@ -14,106 +14,128 @@ import json
 # Save today's buys and sells
 dynamodb = boto3.resource('dynamodb')
 
+
 # Implement trading strategy here
 # Strategy Idea 1:
 # 1. Buy the stock with the most common buy rating
 # 2. Sell entire portfolio the next morning and repeat
-def trade(money, currentDate):
-    equities = scraper.getTodaysEquities(currentDate)
-    equitiesBuyRating = []
-    stocks = dynamodb.Table('Stocks')
+class Trader(object):
 
-    for equity in equities:
-        if str(equity.currentRating) == " Strong Buy" or str(equity.currentRating) == " Outperform" or str(equity.currentRating) == " Sector Outperform" or str(equity.currentRating) == " Mkt Outperform":
-            equitiesBuyRating.append(str(equity.ticker))
+    def __init__(self):
+        self.deltaDays = 0
 
-    # Most common BUY Rating
-    mostCommonBuyTicker = max(set(equitiesBuyRating), key=equitiesBuyRating.count)
-    # priceOfShare = float(getQuotes(mostCommonBuyTicker)[0]['LastTradePrice'])
-    share = Share(str(mostCommonBuyTicker))
-    priceOfShare = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+    def trade(self, money, currentDate):
+        equities = scraper.getTodaysEquities(currentDate)
+        equitiesBuyRating = []
+        stocks = dynamodb.Table('Stocks')
 
-    # Compute the number of shares I can buy
-    numberOfShares = math.floor(money/priceOfShare)
+        for equity in equities:
+            if str(equity.currentRating) == " Strong Buy" or str(equity.currentRating) == " Outperform" or str(equity.currentRating) == " Sector Outperform" or str(equity.currentRating) == " Mkt Outperform":
+                equitiesBuyRating.append(str(equity.ticker))
 
-    # Timestamp
-    today = currentDate
-    timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
+        # Most common BUY Rating
+        if len(equitiesBuyRating) != 0:
+            try:
+                mostCommonBuyTicker = max(set(equitiesBuyRating), key=equitiesBuyRating.count)
+                # priceOfShare = float(getQuotes(mostCommonBuyTicker)[0]['LastTradePrice'])
+                share = Share(str(mostCommonBuyTicker))
+                priceOfShare = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
 
-    # Save to db
-    response = stocks.put_item(
-        Item={
-            'ticker':mostCommonBuyTicker,
-            'timestamp':timestamp,
-            'price':decimal.Decimal(str(priceOfShare)),
-            'shares':decimal.Decimal(numberOfShares),
-            'transactionType':'BUY',
-            'bank':decimal.Decimal(str(money))
-        }
-    )
+                # Compute the number of shares I can buy
+                numberOfShares = math.floor(money/priceOfShare)
 
-# Sell entire portfolio
-def sell(currentDate):
-    stocks = dynamodb.Table('Stocks')
+                # Timestamp
+                today = currentDate
+                timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
 
-    today = currentDate
-    timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 1)
+                # Save to db
+                response = stocks.put_item(
+                    Item={
+                        'ticker':mostCommonBuyTicker,
+                        'timestamp':timestamp,
+                        'price':decimal.Decimal(str(priceOfShare)),
+                        'shares':decimal.Decimal(numberOfShares),
+                        'transactionType':'BUY',
+                        'bank':decimal.Decimal(str(money))
+                    }
+                )
+                self.deltaDays = 0
+            except Exception as e:
+                # print e
+                self.deltaDays = self.deltaDays + 1
+        else:
+            self.deltaDays = self.deltaDays + 1
 
-    # January
-    # if (currentDate.day in [9, 16, 23, 30]):
-    # February/March:
-    if (currentDate.day in [6, 13, 20, 27]):
-        timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 3)
+    # Sell entire portfolio
+    def sell(self, currentDate):
+        stocks = dynamodb.Table('Stocks')
 
-    # January
-    # if (currentDate.day in [17]):
-    # February:
-    # if (currentDate.day in [21]):
-    #     timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day - 4)
-
-    try:
-        response = stocks.get_item(
-            Key={
-                'timestamp': timestamp,
-                'transactionType':'BUY'
-            }
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        item = response['Item']
-
-        numberShares = item['shares']
-        # lastTradePrice = getQuotes(str(item['ticker']))[0][u'LastTradePrice']
-        share = Share(str(item['ticker']))
-        lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
-
-        amountIMade = float(lastTradePrice) * float(numberShares) - float(item['price']) * float(numberShares)
-        print("Net gain: " + str(amountIMade))
-        valueInBank = float(lastTradePrice) * float(numberShares) + (float(item['bank']) - float(numberShares) * float(item['price']))
-        print("Bank: " + str(valueInBank))
-
-        # Timestamp
-        today = currentDate
+        today = currentDate - timedelta(days=(1+self.deltaDays))
         timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
 
-        # Save to db
-        response = stocks.put_item(
-            Item={
-                'ticker':str(item['ticker']),
-                'timestamp':timestamp,
-                'price':decimal.Decimal(str(lastTradePrice)),
-                'shares':decimal.Decimal(numberShares),
-                'transactionType':'SELL',
-                'bank':decimal.Decimal(str(valueInBank))
-            }
-        )
+        if (currentDate.weekday() == 0):
+            today = currentDate - timedelta(days=(3+self.deltaDays))
+            timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
 
-        trade(valueInBank, currentDate)
+        # print timestamp
+
+        try:
+            response = stocks.get_item(
+                Key={
+                    'timestamp': timestamp,
+                    'transactionType':'BUY'
+                }
+            )
+        except ClientError as e:
+            # print(e.response['Error']['Message'])
+            test = 'hello'
+        else:
+            # print currentDate
+            # print timestamp
+            item = response['Item']
+
+
+            numberShares = item['shares']
+            # lastTradePrice = getQuotes(str(item['ticker']))[0][u'LastTradePrice']
+            share = Share(str(item['ticker']))
+
+            try:
+                lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+            except YQLResponseMalformedError as e:
+                if (currentDate.weekday() == 0):
+                    self.deltaDays = self.deltaDays + 3
+                else:
+                    self.deltaDays = self.deltaDays + 1
+                return
+
+            lastTradePrice = float(share.get_historical(str(currentDate), str(currentDate))[0]['Open'])
+
+            amountIMade = float(lastTradePrice) * float(numberShares) - float(item['price']) * float(numberShares)
+            # print("Net gain: " + str(amountIMade))
+            valueInBank = float(lastTradePrice) * float(numberShares) + (float(item['bank']) - float(numberShares) * float(item['price']))
+            print(str(valueInBank))
+
+            # Timestamp
+            today = currentDate
+            timestamp = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
+
+            # Save to db
+            response = stocks.put_item(
+                Item={
+                    'ticker':str(item['ticker']),
+                    'timestamp':timestamp,
+                    'price':decimal.Decimal(str(lastTradePrice)),
+                    'shares':decimal.Decimal(numberShares),
+                    'transactionType':'SELL',
+                    'bank':decimal.Decimal(str(valueInBank))
+                }
+            )
+
+            self.trade(valueInBank, currentDate)
 
 # Trade - January
 # initialDate = date(2017, 1, 3)
-# initialBank = 3000
+# initialBank = 1000
 # trade(initialBank, initialDate)
 # for day in range(4, 32) :
 #     if day not in [7, 8, 14, 15, 16, 21, 22, 28, 29]:
@@ -122,18 +144,34 @@ def sell(currentDate):
 
 # Trade - February
 # initialDate = date(2017, 2, 1)
-# initialBank = 3655.879601
+# initialBank = 1000
 # trade(initialBank, initialDate)
 # for day in range(2, 29) :
 #     if day not in [4, 5, 11, 12, 18, 19, 20, 25, 26]:
 #         currentDate = date(2017, 2, day)
 #         sell(currentDate)
 
-# # Trade - March
-initialDate = date(2017, 3, 1)
-initialBank = 1000
-trade(initialBank, initialDate)
-for day in range(2, 32) :
-    if day not in [4, 5, 11, 12, 18, 19, 25, 26]:
-        currentDate = date(2017, 3, day)
-        sell(currentDate)
+# Trade - March
+# initialDate = date(2017, 3, 1)
+# initialBank = 75000
+# trade(initialBank, initialDate)
+# for day in range(2, 32) :
+#     if day not in [4, 5, 11, 12, 18, 19, 25, 26]:
+#         currentDate = date(2017, 3, day)
+#         sell(currentDate)
+
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+trader = Trader()
+
+start = date(2016, 4, 8)
+trader.trade(1000, start)
+start_date = date(2016, 4, 9)
+end_date = date(2017, 10, 8)
+for single_date in daterange(start_date, end_date):
+    if single_date.weekday() < 5:
+        # print single_date.strftime("%Y-%m-%d")
+        trader.sell(single_date)
